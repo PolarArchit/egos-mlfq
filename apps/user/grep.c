17b0ebb6dbee5d1f51fb6b946bfe021ff5d4f1bd
Archit Chawla <145910868+PolarArchit@users.noreply.github.com>
2025-11-23 19:03:58 +0530

    added code clarity
    Added comments to clarify error handling and tokenization logic.

--- apps/user/grep.c
+++ apps/user/grep.c
@@ -6,7 +6,7 @@ int main(int argc, char** argv) {
         INFO("usage: grep [PATTERN] [FILE]");
         return -1;
     }
-
+    
     int file_ino = dir_lookup(workdir_ino, argv[2]);
     if (file_ino < 0) {
         INFO("grep: file %s not found", argv[2]);
@@ -16,11 +16,11 @@ int main(int argc, char** argv) {
     char buf[BLOCK_SIZE + 1];
     int offset = 0;
     while (1) {
-        if (file_read(file_ino, offset, buf) == -1) {
+        if (file_read(file_ino, offset, buf) == -1) { // if the offset is too large, it returns -1 and also prints !!TDERROR OFFSET TOO LARGE, which should be ignored
             break;
         }
         buf[BLOCK_SIZE] = '\0';
-        char* token = strtok(buf, "\n");    
+        char* token = strtok(buf, "\n");    // split on line, and check every line line is done newline character.
         while (token != NULL) {
             char* line = strstr(token, argv[1]);
             if (line != NULL) {
